d66aae15cb0a8c51ecb4e84081614f89f0b75f8d
pmadhurn <pmadhurn@gmail.com>
2025-01-09 17:19:20 +0530

    Add About.jsx

---
+++ src/Components/About.jsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+  return (
+    <div className="content">
+
+            <h1>Hello this is my Test page where i will try the website first and then i will apply it on production.</h1>
+
+    </div>
+  )
+}
+
+export default About
