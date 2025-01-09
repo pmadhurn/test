7801f3a3a6d64345d2283f4e610a9be9b9b8633e
pmadhurn <pmadhurn@gmail.com>
2025-01-09 17:19:20 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <div className="navbar">
+        <ul className="ulist">
+          <NavLink to="about" className={'navtext'}>About</NavLink>
+          <NavLink to="Api1" className={'navtext'}>api for user tablex</NavLink>
+
+        </ul>
+      </div>
+    </>
   )
 }
 
